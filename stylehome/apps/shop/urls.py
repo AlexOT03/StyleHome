ab17e1dcee52760a07404b4ca6cc4717f6d4c436
AlexOT03 <alexot855@gmail.com>
2024-08-07 10:45:28 -0600

    update shop view and page

--- stylehome/apps/shop/urls.py
+++ stylehome/apps/shop/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path, re_path
 from stylehome.apps.shop import views
 
 urlpatterns = [
-    path('shop/', views.ShopView.as_view(), name='shop'),
     path('shop/product/<int:pk>/', views.ProductDetailView.as_view(), name='product'),
+    path('shop/', views.ShopView.as_view(), name='shop'),
+    re_path(r'^shop/(?P<category_id>.*)?$', views.ShopView.as_view(), name='shop_with_category'),
 ]
