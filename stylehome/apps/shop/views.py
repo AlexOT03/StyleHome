bfef7fe690f8246d42df9cd539c13438518f9135
AlexOT03 <alexot855@gmail.com>
2024-08-06 21:53:33 -0600

    add functionality to shop page

--- stylehome/apps/shop/views.py
+++ stylehome/apps/shop/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from stylehome.apps.shop.models import Product
 
 # Create your views here.
 class ShopView(TemplateView):
@@ -10,4 +11,21 @@ class ShopView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        products = Product.objects.all()
+        context['products'] = products
+        return context
+
+
+class ProductDetailView(TemplateView):
+    """
+    Displays the product detail page
+    """
+    template_name = 'product.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        product = Product.objects.get(pk=self.kwargs['pk'])
+        similar_products = Product.objects.filter(category_id=product.category_id).exclude(id=product.id)
+        context['product'] = product
+        context['similar_products'] = similar_products
         return context
