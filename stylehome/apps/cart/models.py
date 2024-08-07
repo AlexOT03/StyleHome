35f03c7574031c3f6a0b735047eb2aecfad87621
joelCHN <joelchable@outlook.com>
2024-08-07 15:23:00 -0600

    cambio del modelo

--- stylehome/apps/cart/models.py
+++ stylehome/apps/cart/models.py
@@ -5,6 +5,7 @@ from stylehome.apps.client.models import User
 class Carshop (models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+    units = models.IntegerField()
     
     def __str__(self):
         return f"Carshop {self.id}"
