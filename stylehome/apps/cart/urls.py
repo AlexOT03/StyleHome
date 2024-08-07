ba02ccfe674bda0b6ac84cbc9b91136a00d6f94f
joelCHN <joelchable@outlook.com>
2024-08-07 16:11:33 -0600

    add cart funtionality

--- stylehome/apps/cart/urls.py
+++ stylehome/apps/cart/urls.py
@@ -3,4 +3,5 @@ from stylehome.apps.cart import views
 
 urlpatterns = [
     path('cart/', views.CartView.as_view(), name='cart'),
+    path('cart/delete/<int:id>/', views.DeleteCartView.as_view(), name='delete-cart'),
 ]
--- stylehome/apps/cart/views.py
+++ stylehome/apps/cart/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import TemplateView, DeleteView, View
+from stylehome.apps.cart.models import Carshop
 
 # Create your views here.
 class CartView(TemplateView):
@@ -7,4 +8,13 @@ class CartView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        return context
+        context['products'] = Carshop.objects.all().prefetch_related('product_id')
+        return context 
+    
+
+class DeleteCartView(DeleteView):
+    
+    def get(self, request,id):
+        carshop = get_object_or_404(Carshop, id=id)
+        carshop.delete()
+        return redirect('cart')
