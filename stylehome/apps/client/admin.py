9a6537a0fde31eca14e9a8e596dfc1caa6b59951
Gene7sis <genesisraneroo@gmail.com>
2024-08-06 19:50:53 -0600

    modelo de usuarios y relacionados

--- stylehome/apps/client/admin.py
+++ stylehome/apps/client/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import State, Municipality, TypeWorker, User, Supplier
 # Register your models here.
+    
+admin.site.register(State)
+admin.site.register(Municipality)
+admin.site.register(User)
+admin.site.register(TypeWorker)
+admin.site.register(Supplier)
--- stylehome/apps/client/models.py
+++ stylehome/apps/client/models.py
@@ -1,3 +1,72 @@
 from django.db import models
+from django.core.validators import EmailValidator, RegexValidator
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+class State(models.Model):
+    name = models.CharField(max_length=100)
+    abbreviation = models.CharField(max_length=50, blank=True, null=True)
+    
+    def __str__(self):
+        return self.name
+    
+class Municipality(models.Model):
+    state_id = models.ForeignKey(State, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+    
+class User(models.Model):
+    WORK_CHOICES = (
+        ('Cliente', 'Cliente'),
+        ('Vendedor', 'Vendedor'),
+        ('Proveedor', 'Proveedor'),
+        ('Mantenimiento', 'Mantenimiento'),
+    )
+    
+    type_user = models.CharField(choices=WORK_CHOICES, max_length=50, blank=True, default='Cliente')
+    municipality = models.ForeignKey(Municipality, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(validators=[EmailValidator()], unique=True, max_length=50)
+    address = models.CharField(max_length=100, blank=True, null=True)
+    phone = models.CharField(max_length=15, validators=[RegexValidator(r"^\+?1?\d{9,15}$")], blank=True, null=True)
+    photo = models.ImageField(null=True, blank=True, upload_to='profiles/')
+    
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+    
+class TypeWorker(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    type_work = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
+    salary = models.FloatField(default=0.0)
+    
+    def __str__(self):
+        return f"{self.type_work} (ID: {self.user_id.first_name})"
+    
+class Supplier(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    company = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return f"{self.company} (ID: {self.user_id.first_name})"
+
+@receiver(post_save, sender=User)
+def create_related_model(sender, instance, created, **kwargs):
+    if created:
+        if instance.type_user in ['Vendedor', 'Mantenimiento']:
+            TypeWorker.objects.create(
+                user_id=instance,
+                type_work=instance.type_user,
+                description='Descripción por defecto',
+                salary=0.0
+            )
+        elif instance.type_user == 'Proveedor':
+            Supplier.objects.create(
+                user_id=instance,
+                company='Nombre de la empresa por defecto',
+                description='Descripción por defecto'
+            )
